17d10346b9dd45ac1282fe25e6b8c00e8ff1de79
kirill1881 <82600534+kirill1881@users.noreply.github.com>
2022-08-01 22:19:25 +0300

    123

---
+++ README.md
@@ -0,0 +1,6 @@
+# Todowoo web site
+Web site to create ToDo list for everyday tasks
+
+Sign Up,create new task and start you're work day!
+
+Enjoy!
---
+++ todo/admin.py
@@ -0,0 +1,9 @@
+from django.contrib import admin
+from .models import Todo
+
+
+class Todo_admin(admin.ModelAdmin):
+    readonly_fields = ('created',)
+
+
+admin.site.register(Todo, Todo_admin)
